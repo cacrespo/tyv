6817b89f86823d8684d309fa50f134d5fbe4e30e
Carlos A. Crespo <lvccrespo@gmail.com>
2021-06-27 19:02:06 -0300

    ajuste header js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
+// header
+document.querySelector("#encabezado").innerHTML = ''+
+'<div class="sticky">'+
+'<header class="animate__animated animate__bounce animate__delay-2s">'+
+'    <h1><a href="index.html"> 👶 Pañalera T & V 👶 </a></h1>'+
+'    <p><a href="https://api.whatsapp.com/send?phone=1122334455"><i class="fab fa-whatsapp"></i> 11 22 33 44 55</a></p>'+
+'    <nav>'+
+'        <a href="productos.html">Productos</a>'+
+'        <a href="pedidos.html">Pedidos</a>'+
+'        <a href="amigos.html">Amigos</a>'+
+'        <a href="nosotros.html">Nosotros</a>'+
+'    </nav>'
+'</header>'
+'</div>'
+
+
 // footer
 document.querySelector("footer").innerText = "Hecho con mucho ❤️ y poco 💰"
